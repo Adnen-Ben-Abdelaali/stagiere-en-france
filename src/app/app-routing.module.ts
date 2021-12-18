6026333a3644f118b8def24c2fd688a6222f41b6
Adnen-Ben-Abdelaali <benabdelaaliadnen@gmail.com>
2021-12-18 10:02:51 +0100

    routing initialisé

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {IndexSiteComponent} from "./index-site/index-site.component";
+import {ChoixDeStagesParSpecialiteComponent} from "./choix-de-stages-par-specialite/choix-de-stages-par-specialite.component";
+import {InterfaceAdminComponent} from "./interface-admin/interface-admin.component";
+import {LoginBootstrapStyleComponent} from "./login-bootstrap-style/login-bootstrap-style.component";
+import {PagePrincipaleDuStageSelectionneComponent} from "./page-principale-du-stage-selectionne/page-principale-du-stage-selectionne.component";
+import {SignupFormBootstrapComponent} from "./signup-form-bootstrap/signup-form-bootstrap.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'choixDeStagesParSpecialite', component: ChoixDeStagesParSpecialiteComponent
+  },
+  {
+    path:'indexSite', component: IndexSiteComponent
+  },
+  {
+    path:'interfaceAdmin', component: InterfaceAdminComponent
+  },
+  {
+    path:'loginBootstrapStyle', component: LoginBootstrapStyleComponent
+  },
+  {
+    path:'pagePrincipaleDuStageSelectionne', component: PagePrincipaleDuStageSelectionneComponent
+  },
+  {
+    path:'signupFormBootstrap', component: SignupFormBootstrapComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
